d5492095644abd2b88dfa9ecef52a6d62ba88a83
Pasitcodingclass <111564022+Pasitcodingclass@users.noreply.github.com>
2023-10-08 09:12:38 +0700

    Update shopping.js

--- assignment8/shopping.js
+++ assignment8/shopping.js
@@ -1,30 +1,34 @@
 window.onload = pageLoad;
 
 function pageLoad(){
-	var xml = new XMLHttpRequest();
-    xml.open("GET", "cloth.json");
-    xml.onload = function(){
+	var xhr = new XMLHttpRequest();
+    xhr.open("GET", "cloth.json");
+    xhr.onload = function(){
         var jsdata = JSON.parse(xhr.responseText);
         showData(jsdata);
     }
-    xml.onerror = function() { alert("ERROR!"); }; 
-    xml.send();
+    xhr.onerror = function() { alert("ERROR!"); }; 
+    xhr.send();
 }
 
 function showData(data){
-	var keys = Object.keys(data);
+    var keys = Object.keys(data);
     for(var i =0; i< keys.length; i++){
         var temp = document.getElementById("layer");
         var list = temp.querySelectorAll("div");
+
         var image = document.createElement("img");
         image.src = "pic/"+ data[keys[i]].pic;
+
         var brandname = document.createElement("p");
         brandname.innerText = data[keys[i]].brandname;
+
         var price = document.createElement("p");
         price.innerText = data[keys[i]].price;
+
         list[i].appendChild(image);
         list[i].appendChild(brandname);
         list[i].appendChild(price);
+        
     }
 }
-
